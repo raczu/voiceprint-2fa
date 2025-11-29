23a7701e843b49f01ee87f9d5b0d688286408738
raczu <raczuekk@gmail.com>
2025-11-29 17:28:01 +0100

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from voiceprint-2fa!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-2xl m-6>">Hello, World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
